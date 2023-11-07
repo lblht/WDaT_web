89cdc9705b3bb1432e098bb71b043c9771706665
lblht <lehota.lubomir@gmail.com>
2023-11-07 18:49:27 +0100

    ffvv

--- scripts.js
+++ scripts.js
@@ -7,3 +7,21 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 })
+
+
+function showDropdown() {
+    document.getElementById("service-dropdown").classList.toggle("show");
+}
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
